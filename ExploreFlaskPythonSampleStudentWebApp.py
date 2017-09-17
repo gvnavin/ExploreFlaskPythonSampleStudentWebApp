43abbd6e035b02ab121d3786057e865a76b18b9d
Navin G V <gnavin@amazon.com>
2017-09-17 20:54:47 +0530

    adding git ignore, sample form with name, age, address to store in mysql db using sql alchemy installed below components to make this working export PATH=$PATH:/usr/local/mysql/bin sudo pip install mysqlclient sudo pip install mysql-python sudo pip install flask-mysql sudo pip install sqlalchemy sudo pip install Flask-SQLAlchemy used sql work bench as mysql client

--- ExploreFlaskPythonSampleStudentWebApp.py
+++ ExploreFlaskPythonSampleStudentWebApp.py
@@ -1,12 +1,57 @@
-from flask import Flask
+from flask import Flask, flash, render_template, json, request
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "secretkey"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:password@localhost/student_info'
+db = SQLAlchemy(app)
+db.init_app(app)
+
+
+class Student(db.Model):
+
+    __tablename__ = "student"
+
+    name = db.Column(db.String(100), primary_key=True)
+    age = db.Column(db.Integer)
+    address = db.Column(db.String(500))
+
+    def __init__(self, name, age, address):
+        self.name = name
+        self.age = age
+        self.address = address
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/signUp', methods=['POST', 'GET'])
+def signUp():
+    print(request.form)
+    print(request.method)
+    try:
+        name = request.form['name']
+        age = request.form['age']
+        address = request.form['address']
+
+        print("name="+name)
+        print("age="+age)
+        print("address="+address)
+
+        student = Student(name=name, age=age, address=address)
+
+        db.session.add(student)
+        db.session.commit()
+        print('Student information is stored in db')
+
+    except Exception as e:
+        return json.dumps({'error': str(e)})
+
+    return "Student Information Stored Successfully"
 
 
 if __name__ == '__main__':
+    app.debug = True
     app.run()
